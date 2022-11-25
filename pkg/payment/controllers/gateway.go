2afc550fa7fa39947f84ae8304e06fa744986037
Andor Hajnal <hajnal.andor1993@gmail.com>
2022-11-25 15:29:38 +0100

    finish payment controllers

--- pkg/payment/controllers/gateway.go
+++ pkg/payment/controllers/gateway.go
@@ -3,14 +3,72 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/infiniteloopcloud/webshop-poc-ddd/pkg/payment"
 	"github.com/infiniteloopcloud/webshop-poc-ddd/types"
+	"github.com/infiniteloopcloud/webshop-poc-ddd/types/filters"
+	"github.com/infiniteloopcloud/webshop-poc-ddd/utils/httpio"
 )
 
 func SetupGateway(w http.ResponseWriter, r *http.Request) {
-	gw := &types.Gateway{}
-	if err := payment.NewGateway().Setup(r.Context(), gw); err != nil {
-		// error response
+	req, err := httpio.Bind[types.Gateway](r.Body)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
 	}
-	// created response
+	if err := payment.NewGateway().Setup(r.Context(), &req); err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, req)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	req, err := httpio.Bind[types.Gateway](r.Body)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	if err := payment.NewGateway().Update(r.Context(), &req); err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, req)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if err := payment.NewGateway().Delete(r.Context(), id); err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, nil)
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	f, err := httpio.Bind[filters.Gateway](r.Body)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	resp, err := payment.NewGateway().Get(r.Context(), &f)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, resp)
+}
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	f, err := httpio.Bind[filters.Gateway](r.Body)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	resp, err := payment.NewGateway().GetAll(r.Context(), &f)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, resp)
 }
--- pkg/payment/controllers/transaction.go
+++ pkg/payment/controllers/transaction.go
@@ -3,35 +3,50 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/infiniteloopcloud/webshop-poc-ddd/pkg/account"
 	"github.com/infiniteloopcloud/webshop-poc-ddd/pkg/payment"
 	"github.com/infiniteloopcloud/webshop-poc-ddd/types"
 	"github.com/infiniteloopcloud/webshop-poc-ddd/types/filters"
+	"github.com/infiniteloopcloud/webshop-poc-ddd/utils/httpio"
 )
 
 func CreateTransaction(w http.ResponseWriter, r *http.Request) {
-	tx := &types.Transaction{}
-	if err := payment.NewTransaction().Create(r.Context(), tx); err != nil {
-		// error response
+	req, err := httpio.Bind[types.Transaction](r.Body)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
 	}
-	// created response
+	if err := payment.NewTransaction(account.NewMerchant(), account.NewUser()).Create(r.Context(), &req); err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, req)
 }
 
 func GetTransaction(w http.ResponseWriter, r *http.Request) {
-	f := &filters.Transaction{}
-	tx, err := payment.NewTransaction().Get(r.Context(), f)
+	f, err := httpio.Bind[filters.Transaction](r.Body)
 	if err != nil {
-		// error response
+		httpio.ResponseBadRequest(w)
+		return
 	}
-	tx = tx
-	// created response
+	tx, err := payment.NewTransaction(account.NewMerchant(), account.NewUser()).Get(r.Context(), &f)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, tx)
 }
 
 func GetAllTransactions(w http.ResponseWriter, r *http.Request) {
-	f := &filters.Transaction{}
-	txs, err := payment.NewTransaction().GetAll(r.Context(), f)
+	f, err := httpio.Bind[filters.Transaction](r.Body)
 	if err != nil {
-		// error response
+		httpio.ResponseBadRequest(w)
+		return
 	}
-	txs = txs
-	// created response
+	tx, err := payment.NewTransaction(account.NewMerchant(), account.NewUser()).GetAll(r.Context(), &f)
+	if err != nil {
+		httpio.ResponseBadRequest(w)
+		return
+	}
+	httpio.ResponseSuccess(w, tx)
 }
